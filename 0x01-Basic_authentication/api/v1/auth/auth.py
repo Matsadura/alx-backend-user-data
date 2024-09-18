bd339026861e04ab37cdef67ebf8c46f1175076f
Matsadura <zidanditoo1@gmail.com>
2024-09-18 10:41:28 +0100

    [ADD] Task 5: Request validation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,13 +13,20 @@ class Auth:
             return True
         if excluded_paths is None or excluded_paths == '':
             return True
-        if path in excluded_paths or path + '/' in excluded_paths:
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
             return False
         return True
 
     def authorization_header(self, request=None) -> str:
         """Authorization_header public method"""
-        return None
+        if request is None:
+            return None
+        data = request.headers
+        if "Authorization" not in data:
+            return None
+        return data['Authorization']
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Current_user public method"""

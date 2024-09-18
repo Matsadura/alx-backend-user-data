f827a05133a34b10346a7bbef7fd9d842fb2dac3
Matsadura <zidanditoo1@gmail.com>
2024-09-18 10:06:19 +0100

    [FIX] PyCodeStyle

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,15 +6,15 @@ from typing import List, TypeVar
 
 class Auth:
     """A class to manage the API authentication"""
-    
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Require_auth public method"""
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """Authorization_header public method"""
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """Current_user public method"""
         return None

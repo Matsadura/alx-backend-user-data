69803054adc63270e71abef862d795465d0539ec
Matsadura <zidanditoo1@gmail.com>
2024-09-18 13:28:30 +0100

    [UPDATE] Check len of list

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -50,7 +50,7 @@ class BasicAuth(Auth):
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
         user = User.search({'email': user_email})
-        if not user:
+        if not user or len(user) == 0:
             return None
         if not user[0].is_valid_password(user_pwd):
             return None

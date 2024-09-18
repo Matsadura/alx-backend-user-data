dfe7056b7a63f8fe222467f58dd378b5052946a5
Matsadura <zidanditoo1@gmail.com>
2024-09-18 13:35:18 +0100

    [UPDATE] Add try except block for user.search()

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -49,7 +49,10 @@ class BasicAuth(Auth):
             return None
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
-        user = User.search({'email': user_email})
+        try:
+            user = User.search({'email': user_email})
+        except Exception:
+            return None
         if not user or len(user) == 0:
             return None
         if not user[0].is_valid_password(user_pwd):

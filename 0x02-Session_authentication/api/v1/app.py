19faa8e90d110a66ea1ff2b37f4edf1fb16eb8a0
Matsadura <zidanditoo1@gmail.com>
2024-09-19 14:02:08 +0100

    [UPDATE] Delete excess if statement

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -73,8 +73,6 @@ def handle_request():
                       '/api/v1/forbidden/']
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.authorization_header(request) is None:
-        abort(401)
     if auth.authorization_header(request) is None \
             and auth.session_cookie(request) is None:
         abort(401)

278b889dfa439753bb33601ad7c75f7b3d007b82
Matsadura <zidanditoo1@gmail.com>
2024-09-19 14:57:12 +0100

    [FIX] Possible fix

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -9,25 +9,46 @@ from flask import request, jsonify
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login() -> str:
     """Login route"""
-    email = request.form.get('email')
-    if not email or email == '':
+#    email = request.form.get('email')
+#    if not email or email == '':
+#        return jsonify({"error": "email missing"}), 400
+#
+#    password = request.form.get('password')
+#    if not password or password == '':
+#        return jsonify({"error": "password is missing"}), 400
+#
+#    user = User.search({'email': email})
+#    if not user or len(user) == 0:
+#        return jsonify({"error": "no user found for this email"}), 404
+#
+#    valid = user[0].is_valid_password(password)
+#    if not valid:
+#        return jsonify({"error": "wrong password"}), 401
+#
+#    from api.v1.app import auth
+#    session_id = auth.create_session(user[0].id)
+#    session_name = os.getenv('SESSION_NAME')
+#    resp = jsonify(user[0].to_json())
+#    resp.set_cookie(session_name, session_id)
+#    return resp
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not email:
         return jsonify({"error": "email missing"}), 400
 
-    password = request.form.get('password')
-    if not password or password == '':
-        return jsonify({"error": "password is missing"}), 400
+    if not password:
+        return jsonify({"error": "password missing"}), 400
 
-    user = User.search({'email': email})
-    if not user or len(user) == 0:
+    user = User.search({"email": email})
+    if not user:
         return jsonify({"error": "no user found for this email"}), 404
 
-    valid = user[0].is_valid_password(password)
-    if not valid:
+    user = user[0]
+    if not user.is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
-
     from api.v1.app import auth
-    session_id = auth.create_session(user[0].id)
-    session_name = os.getenv('SESSION_NAME')
-    resp = jsonify(user[0].to_json())
-    resp.set_cookie(session_name, session_id)
-    return resp
+    session_id = auth.create_session(user.id)
+    out = jsonify(user.to_json())
+    out.set_cookie(os.getenv("SESSION_NAME"), session_id)
+    return out

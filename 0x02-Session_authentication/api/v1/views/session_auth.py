da72fd11f1b08e12c28338993fcadf4834ffc5b6
Matsadura <zidanditoo1@gmail.com>
2024-09-19 15:01:04 +0100

    [UPDATE] Cleaner code

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -9,28 +9,6 @@ from flask import request, jsonify
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login() -> str:
     """Login route"""
-#    email = request.form.get('email')
-#    if not email or email == '':
-#        return jsonify({"error": "email missing"}), 400
-#
-#    password = request.form.get('password')
-#    if not password or password == '':
-#        return jsonify({"error": "password is missing"}), 400
-#
-#    user = User.search({'email': email})
-#    if not user or len(user) == 0:
-#        return jsonify({"error": "no user found for this email"}), 404
-#
-#    valid = user[0].is_valid_password(password)
-#    if not valid:
-#        return jsonify({"error": "wrong password"}), 401
-#
-#    from api.v1.app import auth
-#    session_id = auth.create_session(user[0].id)
-#    session_name = os.getenv('SESSION_NAME')
-#    resp = jsonify(user[0].to_json())
-#    resp.set_cookie(session_name, session_id)
-#    return resp
     email = request.form.get("email")
     password = request.form.get("password")
 

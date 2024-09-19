c48f10cfd44e2052d6e065117b7a92c38d23148f
Matsadura <zidanditoo1@gmail.com>
2024-09-19 13:23:04 +0100

    [FIX] Return str instead of uuid obj

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -14,6 +14,6 @@ class SessionAuth(Auth):
             return None
         if not isinstance(user_id, str):
             return None
-        u_id = uuid.uuid4()
-        self.user_id_by_session_id[f"{u_id}"] = user_id
-        return u_id
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id

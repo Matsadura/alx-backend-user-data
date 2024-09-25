ee6d47569a980ed9f8a29a974cfabffc55ef9751
Matsadura <zidanditoo1@gmail.com>
2024-09-25 13:32:52 +0100

    [ADD] Tasl 5: Register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,8 +1,30 @@
 #!/usr/bin/env python3
 """Handle auth"""
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
     """Hash a salted hash"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register an user"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError(f'User {user.email} already exists')
+        except NoResultFound:
+            hashed_pwd = _hash_password(password=password)
+            user = self._db.add_user(email, hashed_pwd)
+        return user

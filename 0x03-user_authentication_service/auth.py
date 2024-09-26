303d3219377996e690458499dbc311ba1101242e
Matsadura <zidanditoo1@gmail.com>
2024-09-26 17:00:48 +0100

    [ADD] Task 18: Update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -76,3 +76,14 @@ class Auth:
         token = _generate_uuid()
         self._db.update_user(user.id, **{'reset_token': token})
         return token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update a password"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        new_hash_pwd = _hash_password(password)
+        self._db.update_user(user.id, **{'hashed_password': new_hash_pwd,
+                                         'reset_token': None})
+        return None

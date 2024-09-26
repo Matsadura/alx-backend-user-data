345bff17d8fd5db9429cbe4f16ea2339d2e0b460
Matsadura <zidanditoo1@gmail.com>
2024-09-26 13:56:54 +0100

    [REFACTOR] Change user_id to session_id

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -41,10 +41,12 @@ def login():
         abort(401)
     elif request.method == 'DELETE':
         session_id = request.cookies.get('session_id')
-        user = AUTH.get_user_from_session_id(session_id)
-        if not user:
+        if not session_id:
             abort(403)
-        AUTH.destroy_session(user.id)
+        # user = AUTH.get_user_from_session_id(session_id)
+        # if not user:
+        #     abort(403)
+        AUTH.destroy_session(session_id)
         return redirect('/')
 
 

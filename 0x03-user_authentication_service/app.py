bd8b292997bfeef1e2c41530858b0b53155ed42c
Matsadura <zidanditoo1@gmail.com>
2024-09-26 16:52:28 +0100

    [ADD] Task 17: Get reset password token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -60,5 +60,17 @@ def profile():
     return jsonify({"email": user.email})
 
 
+@app.route('reset_password', methods=['POST'])
+def get_reset_password_token():
+    """Reset a user password"""
+    email = request.form.get('email')
+    try:
+        user = AUTH._db.find_user_by(email=email)
+    except Exception as e:
+        abort(403)
+    token = AUTH.get_reset_password_token(email)
+    return jsonify({"email": email, "reset_token": token})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")

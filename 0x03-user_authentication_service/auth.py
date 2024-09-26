b88e8538979ce60a02f5769a50560d50c21ee953
Matsadura <zidanditoo1@gmail.com>
2024-09-26 13:21:17 +0100

    [ADD] Task 12: Find user by session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -54,3 +54,11 @@ class Auth:
         data = {'session_id': _generate_uuid()}
         self._db.update_user(user.id, **data)
         return data['session_id']
+
+    def get_user_from_session_id(self, session_id: str) -> User | None:
+        """Return the user or None"""
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        return user
